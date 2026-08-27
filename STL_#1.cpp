2422e3a0da2322d9f8a1c7fa54056965e4c00e51
redmvv <redox6851@gmail.com>
2026-08-27 20:10:01 +0200

    I may quite

--- STL_#1.cpp
+++ STL_#1.cpp
@@ -1,38 +1,48 @@
- #include <bits/stdc++.h>
- using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
  int main() {
-   
+
+  string str; 
+  int sum;
   int n;
-  char s;  
-  cin >> n; 
+  cin >>n; 
 
-  deque<int> ans(1, 0); 
+  while (n--) {
+
+  stack<char> opening;  
+  sum = 0; 
+
+  cin >> str; 
+
+  for (auto c : str) {
+
+  if (c == '(' || c== '[') {
+
+  opening.push(c); 
+
+  }
+
+   else if (c == ')' && !opening.empty() && opening.top() == '(') { 
+
+   sum++;     
+   opening.pop(); 
+
+   }
+
+    else if ( c == ']' && !opening.empty() && opening.top() == '[')  { 
+
+    sum++;     
+   opening.pop(); 
+
+  }
+  }
+
   
-  int pos = 0; 
+  cout << sum << '\n'; 
 
- for (int i = 1; i <= n; ++i) {
-
- cin >> s; 
-
-  if (s == 'L')  {
-
-  ans.insert(ans.begin() + pos, i);
-
- }
-
-  else if (s == 'R') {
-
- ans.insert( ans.begin() + pos + 1 , i);
- pos++;
-
- }
  }
   
- for (auto &it : ans)
-
- cout << it << ' '; 
-
- return 0;
+  return 0; 
 
 }
