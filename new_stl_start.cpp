8a1ce0b146a1bc6f5525e48389059550f8124820
redmvv <redox6851@gmail.com>
2026-08-28 21:14:19 +0200

    hope - O - distinct elements

--- new_stl_start.cpp
+++ new_stl_start.cpp
@@ -1,34 +1,33 @@
-#include <bits/stdc++.h>
-using namespace std;
+ #include <bits/stdc++.h>
+ using namespace std;
+
+// h
 
  int main() { 
 
- int n, x, q, m; 
- cin >> n; 
+ int n, q, x;
+ cin >> n >> q; 
+ 
+ vector<int> nums(n); 
 
-  vector<int> shops;
-   
-  while (n--) {
+  for (int i = 0; i < n; ++i) {
 
-  cin >> x;
-  shops.push_back(x);  
+  cin >> nums[i];
 
- }
+  }
 
+  sort(nums.begin(), nums.end()); 
+  nums.erase(unique(nums.begin(), nums.end()), nums.end()); 
+ 
+  while (q--) {
 
- sort(shops.begin(), shops.end());
+  cin >> x; 
+  int greater = nums.end() - upper_bound(nums.begin(), nums.end(), x); 
+  int less = lower_bound(nums.begin(), nums.end(), x) - nums.begin(); 
+  cout << less << ' ' << greater << '\n'; 
 
- cin >> q;  
+  }
 
- while (q--) {
+  return 0; 
 
- cin >> m; 
-
- auto it = upper_bound(shops.begin(), shops.end(), m);
- cout << it - shops.begin() << '\n'; 
-
- } 
-
-
-    return 0; 
  } 
