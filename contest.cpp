26610a6eee8e51f727539d8f98693bd84b2d1587
redmvv <redox6851@gmail.com>
2026-09-01 06:01:06 +0200

    just git better - P- Merge Intervals

--- contest.cpp
+++ contest.cpp
@@ -5,26 +5,50 @@
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+ 
+  int n; 
+  cin >> n;
   
-  int k; 
-  string s;
-  cin >> s >> k;
+  deque<pair<int, int>> dq(n); 
+  vector<pair<int, int>> ans; 
+  pair<int, int> c = {-1, -1};
 
-  sort(s.begin(), s.end()); 
+  for (int i = 0; i < n; ++i) {
 
-  vector<string> per; 
-  per.reserve(s.size()); 
+  cin >> dq[i].first >> dq[i].second; 
 
- do {
-   
-  per.push_back(s); 
+  }
 
- } 
+  sort(dq.begin(), dq.end()); 
 
-  while (next_permutation(s.begin(), s.end())); 
+  c = dq[0]; 
 
-  cout << per[k-1]; 
-  
+  for (int i = 1; i < dq.size(); ++i) {
+
+ int l = max(c.first, dq[i].first); 
+ int r = min(c.second, dq[i].second); 
+
+ if (l <= r)  {
+
+ c.first = min(c.first, dq[i].first); 
+ c.second = max(c.second, dq[i].second); 
+ 
+ }
+
+ else {
+
+ ans.push_back(c); 
+ c = dq[i]; 
+
+ }
+ }
+
+ ans.push_back(c); 
+
+ for (int i = 0;i < ans.size(); ++i)
+
+ cout << ans[i].first << ' '  << ans[i].second << '\n'; 
+
  return 0; 
 
 
