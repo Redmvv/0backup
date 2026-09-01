8e07ce2aeb6e6a17e88e00cf9bb1cff94073efc3
redmvv <redox6851@gmail.com>
2026-09-01 15:58:15 +0200

    It's weird - E- Pair of Balls

--- contest.cpp
+++ contest.cpp
@@ -6,46 +6,94 @@
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
  
-  int n, x, max = 0;
-  bool valid = true; 
-  cin >> n; 
-  
-  stack<int> st; 
+ int n, m; 
  
- while (n--) {
+ cin >> n >> m; 
 
- cin >> x; 
+ vector<queue<int>> cy(m+1);  
+ vector<array<int, 3>> freq(n+1, {0, -1 , -1});  
+ queue<pair<int, int>> q; 
 
- if (x > max) { 
+ for (int i = 1; i <= m; ++i) {
 
- for (int i = max + 1; i < x; ++i){
+  int k, x; 
+  cin >> k; 
 
-    st.push(i);
+  for (int j = 0; j < k; ++j) {
+
+  cin >> x; 
+  cy[i].push(x); 
 
   }
 
-    max = x; 
+  freq[cy[i].front()][0]++;
+
+ if (freq[cy[i].front()][0] == 2) 
+
+  freq[cy[i].front()][2] = i; 
+   
+  else freq[cy[i].front()][1] = i;
+
+  if (freq[cy[i].front()][0] == 2)  
+
+  q.push({freq[cy[i].front()][1], freq[cy[i].front()][2]}); 
+
+  } 
+
+  while (!q.empty()) {
+
+  auto [i, j] = q.front(); 
+  q.pop(); 
+
+  cy[i].pop();  
+  cy[j].pop();  
+
+  if (!cy[i].empty()) {
+
+  freq[cy[i].front()][0]++;
+
+ if (freq[cy[i].front()][0] == 2) 
+
+  freq[cy[i].front()][2] = i; 
+   
+  else freq[cy[i].front()][1] = i;
+
+  if (freq[cy[i].front()][0] == 2)  
+
+  q.push({freq[cy[i].front()][1], freq[cy[i].front()][2]}); 
+
+  }
+
+ if (!cy[j].empty()) {
+
+  freq[cy[j].front()][0]++;
+
+ if (freq[cy[j].front()][0] == 2) 
+
+  freq[cy[j].front()][2] = j; 
+   
+  else freq[cy[j].front()][1] = j;
+
+  if (freq[cy[j].front()][0] == 2)  
+
+  q.push({freq[cy[j].front()][1], freq[cy[j].front()][2]}); 
+
+  }
+
  }
 
+ bool valid = true; 
 
- else {
+ for (int i = 1; i <= m; ++i) 
 
-  if (st.top() == x)
-
-  st.pop(); 
-
-  else {
-
-    valid = false; 
-    break;
-
-  }
-  }
-
- } 
-  
-  cout << (valid ? "YES" : "NO"); 
+ if (!cy[i].empty()) {
  
+ valid = false; 
+ break;
+
+ }
+
+  cout << (valid ? "Yes\n" : "No\n"); 
 
  return 0; 
  
