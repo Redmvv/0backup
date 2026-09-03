7287a32555e10db94e570d00793e97e9905c56e6
redmvv <redox6851@gmail.com>
2026-09-03 18:20:55 +0200

    per-solved

--- STL_#2.cpp
+++ STL_#2.cpp
@@ -6,76 +6,31 @@
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
- int n, m, k, x, b;
- int cnt = 0; 
- cin >> n >> m >> k; 
+ int q;
+ string qr;  
+ priority_queue<int, vector<int>, greater<int>> pq; 
+ cin >> q; 
 
- vector<pair<int, int>> app(n); 
- map<pair<int, int>, bool> apart; 
- 
- 
-  for (int i = 0; i < n; ++i) {
+ while (q--) {
 
-    cin >> x;
+ cin >> qr; 
 
-    int l = x - k; 
-    int r = x + k; 
-    app[i] = {l, r}; 
+ if (qr == "push") {
 
-    } 
+  int x; 
+  cin >> x; 
+  pq.push(x); 
 
-
- while (m--) {
-
- cin >> b;
- 
-  int l = b - k; 
-  int r = b + k; 
-  apart[{l, r}]; 
- 
  }
 
- 
- 
- for (auto [l, r] : app) {
+ else if (qr == "pop") 
+ pq.pop(); 
 
-    auto it = apart.find({l, r});
+ else if (qr == "top") 
+ cout << pq.top() << '\n';  
 
-    if (it == apart.end()) continue;
-    
-    if(it->second) continue;
-
-    else  {
-    
-    it->second = 1; 
-    cnt++;
-  }
-
- } 
-
- cout << "count = " << cnt << '\n'; 
-
- cout << "\n============\n";
-  
-  for (auto [i, j] : app) {
-
- cout << i << ' ' << j << '\n'; 
-
-  }
-  
-  cout << "\n============\n";
-
-
-  for (auto [i, j] : apart) {
-
- cout << i.first << ' ' << i.second << " --> " << j << '\n'; 
-
-  }
-
-  cout << apart.size() << '\n'; 
-
+ }
 
   return 0;
-
 
  } 
