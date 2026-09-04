8b87ede2d7817ed66aa256816899c18638b16d3b
redmvv <redox6851@gmail.com>
2026-09-04 14:59:57 +0200

    do not count yourself out yet - B - Concert Tickets

--- STL_#2.cpp
+++ STL_#2.cpp
@@ -6,30 +6,69 @@
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
- int q;
- string qr;  
- priority_queue<int, vector<int>, greater<int>> pq; 
- cin >> q; 
+  int n, m, h, t;
+  cin >> n >> m;
 
- while (q--) {
+  multiset<int> ms; 
 
- cin >> qr; 
+  // O(n log(n))
 
- if (qr == "push") {
-
-  int x; 
-  cin >> x; 
-  pq.push(x); 
+  while (n--) { 
+  
+  cin >> h; 
+  ms.insert(h); 
 
  }
 
- else if (qr == "pop") 
- pq.pop(); 
+ // O(m log(n)) 
 
- else if (qr == "top") 
- cout << pq.top() << '\n';  
+  while (m--) {  
+
+  cin >> t;
+
+  auto it = ms.lower_bound(t); // o(log n)
+
+  if (it != ms.end()) {
+
+  if (*it == t) {
+    
+  cout << t << '\n';
+  ms.erase(it); // o(1)
+
+  }
+
+  else if (it == ms.begin()) {
+
+    cout << -1 << '\n';
 
  }
+       
+ else  {
+
+ --it; 
+ cout << *it << '\n';  
+ ms.erase(it);
+
+ }
+ } 
+ 
+  else  {
+
+  if (!ms.empty()) {
+
+  auto i = prev(ms.end()); 
+  cout << *i<< '\n'; 
+  ms.erase(i);
+
+ }
+
+ else cout << -1 << '\n'; 
+ 
+ }
+
+  }
+
+ //  O(n + m)log(n)
 
   return 0;
 
