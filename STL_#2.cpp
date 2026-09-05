345b0bb74c61f1446863393832399d4be3339391
redmvv <redox6851@gmail.com>
2026-09-05 06:59:32 +0200

    we will be back! -F - Registration System

--- STL_#2.cpp
+++ STL_#2.cpp
@@ -6,69 +6,38 @@
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n, m, h, t;
-  cin >> n >> m;
+  int n;
+  string name; 
+  cin >> n;
 
-  multiset<int> ms; 
+  map<string, int> m; 
 
-  // O(n log(n))
+  while (n--) {
 
-  while (n--) { 
-  
-  cin >> h; 
-  ms.insert(h); 
+  cin >> name; 
 
- }
+  auto it = m.find(name); 
 
- // O(m log(n)) 
+ if (it != m.end()) {
 
-  while (m--) {  
+  string new_name = name;
+  string num = to_string(m[name]);  
+  new_name += num; 
+  m[name]++; 
 
-  cin >> t;
+  m[new_name] = 1; 
 
-  auto it = ms.lower_bound(t); // o(log n)
-
-  if (it != ms.end()) {
-
-  if (*it == t) {
-    
-  cout << t << '\n';
-  ms.erase(it); // o(1)
+ cout << new_name << '\n'; 
 
   }
 
-  else if (it == ms.begin()) {
+  else {
 
-    cout << -1 << '\n';
-
- }
-       
- else  {
-
- --it; 
- cout << *it << '\n';  
- ms.erase(it);
-
- }
- } 
+  m[name] = 1; 
+  cout << "OK\n"; 
  
-  else  {
-
-  if (!ms.empty()) {
-
-  auto i = prev(ms.end()); 
-  cout << *i<< '\n'; 
-  ms.erase(i);
-
- }
-
- else cout << -1 << '\n'; 
- 
- }
-
   }
-
- //  O(n + m)log(n)
+  } 
 
   return 0;
 
