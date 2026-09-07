c1b553229050305dfac05429c5be445bb183353b
redmvv <redox6851@gmail.com>
2026-09-07 19:58:39 +0200

    Perfix Sum

--- STL_#2.cpp
+++ STL_#2.cpp
@@ -6,59 +6,6 @@
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n, id; 
-  cin >> n;
-
-  deque<int> shelf; 
-  priority_queue<int> table; 
-
-  while (n--) {
-
-  cin >> id; 
-  shelf.push_back(id);
-
-  }
-
-  int q; 
-  char qt;  
-  cin >> q;
-
-  while (q--) {
-
-  cin >> qt; 
-
-  if (qt == 'L') {
-
-  if (shelf.empty()) continue;
-
-  table.push(shelf.front()); 
-  shelf.pop_front(); 
-
-  }
-
-  else if (qt == 'R') {
-
-  if (shelf.empty()) continue;
-
-  table.push(shelf.back()); 
-  shelf.pop_back(); 
-
-  }
-
-  else {
-
-  if (table.empty()) {
-
-  cout << -1 << '\n'; 
-  continue;
-
-  }
-  
-  cout << table.top() << '\n'; 
-  table.pop();
-  
-  }
-  }
 
 
  return 0; 
