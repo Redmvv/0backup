c3f8706ec6f965f31fbab58551cd7a849819e0e3
redmvv <redox6851@gmail.com>
2026-09-07 08:20:02 +0200

    copy-pasta to learn - S - Order statistic set

--- STL_#2.cpp
+++ STL_#2.cpp
@@ -1,43 +1,79 @@
  #include <bits/stdc++.h>
+ #include<ext/pb_ds/assoc_container.hpp>
+ #include<ext/pb_ds/tree_policy.hpp>
  using namespace std;
+ using namespace __gnu_pbds;
+ 
+ typedef tree<int , null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> OrderedSet;
+ 
+  void erase(OrderedSet &t, int v) {
 
- int main() {
+   int rank = t.order_of_key(v);
+  auto it = t.find_by_order(rank);
+
+   t.erase(it);
+
+ }
+
+ int main() { 
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-   int n, x; 
-   cin >> n;
+  int q;
+  cin >> q; 
+  
+  OrderedSet os; 
+  
+  while (q--) {
 
-  multiset<int> t; 
+   int x; 
+   char op;
 
-  for (int i = 0; i < n; ++i) {
+  cin >> op; 
 
-   cin >> x; 
+  if (op == 'I'){
 
-   if (!t.empty())  {
-
-  auto it = t.upper_bound(x);
-
-  if (it != t.end()) { 
-
-  t.erase(it);
-  t.insert(x);
+  cin >> x; 
+  os.insert(x); 
 
   }
 
-  else t.insert(x); 
+  else if (op == 'D') {
 
-   }
+  cin >> x;
+
+  if (os.order_of_key(x) != os.order_of_key(x + 1))
+
+  erase(os, x); 
+  
+  }
+
+  else if (op == 'K') {
+
+   cin >> x; 
+
+  if (x > os.size()) {
    
- else t.insert(x); 
+   cout << "invalid\n"; 
+   continue;
 
-  } 
+  }
 
+  auto it = os.find_by_order(--x); 
+  cout << *it << '\n'; 
 
-  cout << t.size(); 
+  }
+ 
+  else {
 
+  cin >> x; 
+  cout << os.order_of_key(x) << '\n'; 
 
-  return 0;
+  }
+
+  }
+ 
+ return 0; 
 
  } 
