74115feda4b199d1fa98cd7521981ca8eb52c88a
redmvv <redox6851@gmail.com>
2026-09-07 18:35:13 +0200

    easy book? - J-Aymoon and Books

--- STL_#2.cpp
+++ STL_#2.cpp
@@ -1,79 +1,66 @@
- #include <bits/stdc++.h>
- #include<ext/pb_ds/assoc_container.hpp>
- #include<ext/pb_ds/tree_policy.hpp>
- using namespace std;
- using namespace __gnu_pbds;
+  #include <bits/stdc++.h>
+  using namespace std;
  
- typedef tree<int , null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> OrderedSet;
- 
-  void erase(OrderedSet &t, int v) {
-
-   int rank = t.order_of_key(v);
-  auto it = t.find_by_order(rank);
-
-   t.erase(it);
-
- }
-
- int main() { 
+  int main() { 
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int q;
-  cin >> q; 
-  
-  OrderedSet os; 
-  
+  int n, id; 
+  cin >> n;
+
+  deque<int> shelf; 
+  priority_queue<int> table; 
+
+  while (n--) {
+
+  cin >> id; 
+  shelf.push_back(id);
+
+  }
+
+  int q; 
+  char qt;  
+  cin >> q;
+
   while (q--) {
 
-   int x; 
-   char op;
+  cin >> qt; 
 
-  cin >> op; 
+  if (qt == 'L') {
 
-  if (op == 'I'){
+  if (shelf.empty()) continue;
 
-  cin >> x; 
-  os.insert(x); 
-
-  }
-
-  else if (op == 'D') {
-
-  cin >> x;
-
-  if (os.order_of_key(x) != os.order_of_key(x + 1))
-
-  erase(os, x); 
-  
-  }
-
-  else if (op == 'K') {
-
-   cin >> x; 
-
-  if (x > os.size()) {
-   
-   cout << "invalid\n"; 
-   continue;
+  table.push(shelf.front()); 
+  shelf.pop_front(); 
 
   }
 
-  auto it = os.find_by_order(--x); 
-  cout << *it << '\n'; 
+  else if (qt == 'R') {
+
+  if (shelf.empty()) continue;
+
+  table.push(shelf.back()); 
+  shelf.pop_back(); 
 
   }
- 
+
   else {
 
-  cin >> x; 
-  cout << os.order_of_key(x) << '\n'; 
+  if (table.empty()) {
+
+  cout << -1 << '\n'; 
+  continue;
 
   }
-
+  
+  cout << table.top() << '\n'; 
+  table.pop();
+  
   }
- 
+  }
+
+
  return 0; 
 
  } 
