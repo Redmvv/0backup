fafce3f68fcd5f798077372fd56d9ca236543d87
redmvv <redox6851@gmail.com>
2026-09-09 19:32:21 +0200

    I fully got it thx gpty -Prefix Sum

--- STL_#2.cpp
+++ STL_#2.cpp
@@ -1,13 +1,43 @@
   #include <bits/stdc++.h>
   using namespace std;
- 
+
+
   int main() { 
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+ int n, x; 
+ cin >> n >> x; 
+
+ long long p[n+1] = {}; 
+
+
+  for (int i = 1; i <= n; ++i) {
+
+  cin >> p[i]; 
+  p[i] += p[i - 1]; 
+  
+ }
+
+  long long ans = 0; 
+
+  map<long long, long long> m; 
+
+  for (int i = 0; i <= n; ++i) {
+
+  long long wanted = p[i] - x; 
+ 
+  ans += m[wanted]; 
+  m[p[i]]++;
+ 
+  }
+    
+ cout << ans; 
+  
 
 
  return 0; 
+
 
  } 
